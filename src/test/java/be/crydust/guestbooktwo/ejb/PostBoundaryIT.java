d013c8536d569c4fe1bc622bb2a5e9036d992a1b
Kristof Neirynck <k.neirynck@gmail.com>
2014-01-29 18:23:50 +0100

    unittests with hibernate 4.3 work (using snapshot of openejb-core)

--- src/test/java/be/crydust/guestbooktwo/ejb/PostBoundaryIT.java
+++ src/test/java/be/crydust/guestbooktwo/ejb/PostBoundaryIT.java
@@ -24,10 +24,17 @@ public class PostBoundaryIT {
 
     @BeforeClass
     public static void startTheContainer() {
+
         Properties p = new Properties();
         p.put("testDB", "new://Resource?type=DataSource");
         p.put("testDB.JdbcDriver", "org.h2.Driver");
         p.put("testDB.JdbcUrl", "jdbc:h2:mem:testDB;DB_CLOSE_DELAY=-1");
+        p.put("guestbooktwoPU.openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)");
+        p.put("guestbooktwoPU.eclipselink.ddl-generation", "drop-and-create-tables");
+        p.put("guestbooktwoPU.hibernate.hbm2ddl.auto", "create-drop");
+        p.put("guestbooktwoPU.hibernate.transaction.jta.platform", "org.apache.openejb.hibernate.OpenEJBJtaPlatform2");
+        p.put("guestbooktwoPU.hibernate.transaction.manager_lookup_class", "org.apache.openejb.hibernate.TransactionManagerLookup");
+
         ejbContainer = EJBContainer.createEJBContainer(p);
     }
 
@@ -43,34 +50,34 @@ public class PostBoundaryIT {
         }
     }
 
-//    @Test(expected = Exception.class)
-//    public void testDeleteByWordNull() {
-//        cut.deleteByWord(null);
-//    }
+    @Test(expected = Exception.class)
+    public void testDeleteByWordNull() {
+        cut.deleteByWord(null);
+    }
 
-//    @Test(expected = Exception.class)
-//    public void testDeleteByWordEmpty() {
-//        cut.deleteByWord("");
-//    }
+    @Test(expected = Exception.class)
+    public void testDeleteByWordEmpty() {
+        cut.deleteByWord("");
+    }
 
     @Test
     public void testDeleteByWordNothingToDelete() {
         assertThat(cut.deleteByWord("expletive"), is(0));
     }
 
-//    @Test
-//    public void testDeleteByWordOneToDelete() {
-//        Post post1 = new Post();
-//        post1.setAuthor("tester");
-//        post1.setMessage("message");
-//        cut.create(post1);
-//        assertThat(cut.deleteByWord("expletive"), is(0));
-//        Post post2 = new Post();
-//        post2.setAuthor("tester");
-//        post2.setMessage("message expletive");
-//        cut.create(post2);
-//        assertThat(cut.deleteByWord("expletive"), is(1));
-//        assertThat(cut.deleteByWord("expletive"), is(0));
-//    }
+    @Test
+    public void testDeleteByWordOneToDelete() {
+        Post post1 = new Post();
+        post1.setAuthor("tester");
+        post1.setMessage("message");
+        cut.create(post1);
+        assertThat(cut.deleteByWord("expletive"), is(0));
+        Post post2 = new Post();
+        post2.setAuthor("tester");
+        post2.setMessage("message expletive");
+        cut.create(post2);
+        assertThat(cut.deleteByWord("expletive"), is(1));
+        assertThat(cut.deleteByWord("expletive"), is(0));
+    }
 
 }
