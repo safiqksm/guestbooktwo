2a1e40ca4bc401070dd6629bc9074dbc01c4c04f
Kristof Neirynck <k.neirynck@gmail.com>
2014-03-25 20:33:31 +0100

    begin table implementation
    delete button works client side, but doesn't send delete request yet

--- src/main/webapp/angular/post.js
+++ src/main/webapp/angular/post.js
@@ -1,10 +1,11 @@
-angular
+var app = angular
         .module('postsApp', [])
         .value('postUrl', '../webresources/be.crydust.guestbooktwo.entities.post')
         .controller('PostListCtrl', function($scope, $http, postUrl) {
-            $http
-                    .get(postUrl)
-                    .success(function(data) {
-                        $scope.posts = data;
-                    });
+            $http.get(postUrl).success(function(data) {
+                $scope.posts = data;
+            });
+            $scope.delete = function(post) {
+                $scope.posts.splice($scope.posts.indexOf(post), 1);
+            };
         });
